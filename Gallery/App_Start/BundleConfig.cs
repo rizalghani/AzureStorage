cbc8c877237e25cd943c71b632e6bde714656f2c
rizalghani <m.rizal.ghani@gmail.com>
2019-06-19 16:36:35 +0700

    Update All

--- Gallery/App_Start/BundleConfig.cs
+++ Gallery/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Gallery
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
+                        "~/Scripts/lightbox.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -24,7 +27,8 @@ namespace Gallery
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/lightbox.css"));
         }
     }
 }
